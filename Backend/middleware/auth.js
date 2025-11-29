97f83f99323e74a72c29153ad09bc6a7d27d3c48
MANYA SHUKLA <shuklamanya99@gmail.com>
2025-11-29 15:05:21 +0530

    update

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,15 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    // Get token from header
-    const token = req.header('x-auth-token');
 
-    // Check if not token
+    const token = req.header('x-auth-token');
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
-
-    // Verify token
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET || 'secret');
         req.user = decoded.user;
